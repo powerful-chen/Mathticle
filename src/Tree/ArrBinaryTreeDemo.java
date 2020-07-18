146182525ed9fc65083bd2124bd57cd22645357e
小陈 <1716867307@qq.com>
2020-07-18 14:24:39 +0800

    顺序存储二叉树

---
+++ src/Tree/ArrBinaryTreeDemo.java
@@ -0,0 +1,79 @@
+package Tree;
+
+public class ArrBinaryTreeDemo {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
+//        arrBinaryTree.preOrder();
+//        arrBinaryTree.infixOrder(0);
+        arrBinaryTree.postOrder(0);
+    }
+
+}
+
+//编写一个ArrayBinaryTree，实现顺序存储二叉树遍历
+class ArrBinaryTree {
+
+    private int[] arr;
+
+    public ArrBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    //重载preOrder
+    public void preOrder() {
+        this.preOrder(0);
+    }
+
+
+    /**
+     * @param index 数组的下标
+     */
+    //前序遍历
+    public void preOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        System.out.println(arr[index]);
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(index * 2 + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(index * 2 + 2);
+        }
+    }
+
+    //中序遍历
+    public void infixOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(index * 2 + 1);
+        }
+        System.out.println(arr[index]);
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(index * 2 + 2);
+        }
+    }
+
+    //后序遍历
+    public void postOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+        }
+        //向左递归遍历
+        if ((index * 2 + 1) < arr.length) {
+            preOrder(index * 2 + 1);
+        }
+        //向右递归遍历
+        if ((index * 2 + 2) < arr.length) {
+            preOrder(index * 2 + 2);
+        }
+        System.out.println(arr[index]);
+    }
+}
